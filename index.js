55b23ad4f1596025b298490a15129979f40b099a
ViggoSeerden <491216@student.fontys.nl>
2024-10-31 09:54:52 +0100

    initial

---
+++ index.js
@@ -0,0 +1,31 @@
+const amqplib = require("amqplib");
+
+async function startMessagingInstance() {
+    connection = await amqplib.connect('amqp://rabbitmq:rabbitmq@localhost')
+    channel = await connection.createChannel()
+
+    var queue = 'payment-skeleton';
+
+    channel.assertQueue(queue, {
+        durable: false
+    });
+
+    channel.prefetch(1);
+    console.log('Now listening...');
+    channel.consume(queue, async function reply(msg) {
+
+        console.log(`Received: ${msg.content.toString()}`);
+
+        responseMessage = "Successful Response from Payment Service!"
+
+        await channel.sendToQueue(msg.properties.replyTo,
+            Buffer.from(JSON.stringify(responseMessage)), {
+            correlationId: msg.properties.correlationId
+        });
+
+        console.log(`Responded: ${JSON.stringify(responseMessage)}`);
+
+        channel.ack(msg);
+    });
+}
+startMessagingInstance()
